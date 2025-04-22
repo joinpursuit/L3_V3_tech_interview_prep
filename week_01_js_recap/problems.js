d82c19a794f3be97da5a0a6b6e32cdaf38a11a79
abidh186 <abidhussain@pursuit.org>
2025-04-22 16:52:23 -0400

    chore: week 1 problem set - install jest package - solve problems: reverseString, findMax, isPalindrome - add extra tests for edge cases

--- week_01_js_recap/problems.js
+++ week_01_js_recap/problems.js
@@ -1,16 +1,32 @@
 // Reverse a string
 function reverseString(str) {
+  let reversedStr = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
 
+  return reversedStr;
 }
-  
+
 // Find max in array
 function findMax(arr) {
+  let max = -Infinity;
+  for (el of arr) {
+    if (el > max) {
+      max = el;
+    }
+  }
 
+  return max;
 }
 
 // Check if palindrome
 function isPalindrome(word) {
-    
+  if (word == reverseString(word)) {
+    return true;
+  }
+
+  return false;
 }
 
 module.exports = { reverseString, findMax, isPalindrome };
--- week_01_js_recap/problems.test.js
+++ week_01_js_recap/problems.test.js
@@ -2,13 +2,20 @@ const { reverseString, findMax, isPalindrome } = require('./problems');
 
 test('reverseString reverses a word', () => {
   expect(reverseString('hello')).toBe('olleh');
+  expect(reverseString('x')).toBe('x');
+  expect(reverseString('')).toBe('');
 });
 
 test('findMax returns the largest number', () => {
   expect(findMax([1, 5, 3, 9])).toBe(9);
+  expect(findMax([1, -5, -9])).toBe(1);
+  expect(findMax([1])).toBe(1);
+  expect(findMax([])).toBe(-Infinity);
 });
 
 test('isPalindrome checks for palindrome correctly', () => {
   expect(isPalindrome('racecar')).toBe(true);
   expect(isPalindrome('hello')).toBe(false);
+  expect(isPalindrome('x')).toBe(true);
+  expect(isPalindrome('')).toBe(true);
 });
