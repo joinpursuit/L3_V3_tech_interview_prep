63bf7020289f855f4900a7abc226a4e0c0a0c369
Kenyetta Griffin (She/Her) <96461060+PursuitMadeMe@users.noreply.github.com>
2025-04-24 20:59:11 -0400

    Completed and tested reverseString function

--- week_01_js_recap/problems.js
+++ week_01_js_recap/problems.js
@@ -1,6 +1,28 @@
 // Reverse a string
-function reverseString(str) {
+/**
+Input a string 
+Output string in reverse 
 
+EXAMPLE 
+"pebble" => "elbbep"
+"brooklyn" => "nylkoorb"
+
+EDGE Cases 
+"" => -1
+
+- create a variable to hold the answer
+- loop through in reverse, from the last character 
+- push that character into the empty variable 
+- return the populated variable 
+ */
+function reverseString(str) {
+if (str === "") return -1
+
+    let rString = ''
+    for(let i = str.length-1; i >= 0; i--) {
+        rString += str[i]
+}   
+    return rString 
 }
   
 // Find max in array
