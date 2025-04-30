704cbe66d8a7fe198905aa30b18ed934c8da6139
yanakolajara <yanakolajara@pursuit.org>
2025-04-29 21:13:27 -0400

    solved reverseString problem

--- week_01_js_recap/problems.js
+++ week_01_js_recap/problems.js
@@ -1,16 +1,12 @@
 // Reverse a string
 function reverseString(str) {
-
+  return str.split('').reverse().join('');
 }
-  
+
 // Find max in array
-function findMax(arr) {
-
-}
+function findMax(arr) {}
 
 // Check if palindrome
-function isPalindrome(word) {
-    
-}
+function isPalindrome(word) {}
 
 module.exports = { reverseString, findMax, isPalindrome };
