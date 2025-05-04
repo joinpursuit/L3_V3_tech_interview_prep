faf06c613456e0110c62aec285010573be7e222a
Kenyetta Griffin (She/Her) <96461060+PursuitMadeMe@users.noreply.github.com>
2025-05-03 23:57:40 -0400

    Completed Week 2 Python functions and tests

--- week_02_python_intro/problems.py
+++ week_02_python_intro/problems.py
@@ -1,8 +1,22 @@
 def reverse_string(s):
+    reverse = ""
+    for char in s :
+        reverse = char + reverse
+    return reverse
     pass
 
 def find_max(lst):
+    max_val = 0
+    for num in lst:
+        if num > max_val:
+            max_val = num 
+    return max_val
     pass
 
 def is_palindrome(word):
+    reverse = ""
+    for char in word:
+        reverse = char + reverse 
+    return word == reverse
+
     pass
