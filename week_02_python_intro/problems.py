0a284307173db94f30b30fdbe88369df77ffe17b
abidh186 <abidhussain@pursuit.org>
2025-05-07 13:56:24 -0400

    chore: implement basic problems using python, pass tests

--- week_02_python_intro/problems.py
+++ week_02_python_intro/problems.py
@@ -1,8 +1,20 @@
 def reverse_string(s):
-    pass
+    output = ""
+    for i in range(len(s) - 1, -1, -1):
+        output += s[i]
+    return output
 
 def find_max(lst):
-    pass
+    max_value = lst[0]
+    for i in range(1, len(lst)):
+        if lst[i] > max_value:
+            max_value = lst[i]
+    return max_value    
 
 def is_palindrome(word):
-    pass
+    reversed_word = reverse_string(word)
+    if word == reversed_word:
+        return True
+    else:
+        return False
+    
