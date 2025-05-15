88fcf91d5f56c4dbc239206477958637b47579b3
yanakolajara <yanakolajara@pursuit.org>
2025-05-15 00:10:34 -0400

    created tests for is_palindrome

--- week_03_python_2/test/test_is_palindrome.py
+++ week_03_python_2/test/test_is_palindrome.py
@@ -2,5 +2,12 @@ import pytest
 from problems import is_palindrome
 
 def test_is_palindrome():
-    assert is_palindrome("racecar")
-    assert not is_palindrome("hello")
+  assert is_palindrome("racecar")
+    
+    
+def test_not_is_palindrome():
+  assert not is_palindrome("hello")
+
+def test_empty():
+    assert is_palindrome("")
+   
