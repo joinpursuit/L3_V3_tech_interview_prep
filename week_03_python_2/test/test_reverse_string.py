b6661224af0aff35ba2c2d136f66ba31db181fd4
yanakolajara <yanakolajara@pursuit.org>
2025-05-15 00:02:51 -0400

    created tests for reverse_string

--- week_03_python_2/test/test_reverse_string.py
+++ week_03_python_2/test/test_reverse_string.py
@@ -1,8 +1,17 @@
 import pytest
 from problems import reverse_string
 
-def test_reverse_string():
-    assert reverse_string("hello") == "olleh"
+
+def test_str():
+    for i in ["hello", "a", "FdafdsGb"]:
+        assert reverse_string("hello") == "olleh"
+
+def test_empty_str():
+    assert reverse_string("") == ""
+
+def test_int():
+    with pytest.raises(TypeError):
+        reverse_string(1)
 
 
 
