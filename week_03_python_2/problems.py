3379efe8cf2409162e47d9b4c4d593759b8d86a6
yanakolajara <yanakolajara@pursuit.org>
2025-05-14 23:51:04 -0400

    Refactored solutions

--- week_03_python_2/problems.py
+++ week_03_python_2/problems.py
@@ -1,26 +1,8 @@
-def main():
-    print(reverse_string("hello"))
-    print(find_max([1, 5, 3, 9]))
-    print(is_palindrome("racecar"))
-    print(is_palindrome("hello"))
-
-def reverse_string(s):
-    return s[::-1]
+def reverse_string(str):
+    return str[::-1]
 
 def find_max(lst):
     return lst[-1]
 
-def is_palindrome(word):
-    pointer1 = 0
-    pointer2 = len(word) - 1
-    while pointer1 < pointer2:
-        if word[pointer1] != word[pointer2]:
-            return False
-        pointer1 += 1
-        pointer2 -= 1
-    return True
-
-
-
-if __name__ == "__main__":
-    main()
+def is_palindrome(str):
+    return str == str[::-1]
