dcc6420399f9138ad67ac2fda7cbd75f64cb316c
yanakolajara <yanakolajara@pursuit.org>
2025-05-15 00:06:17 -0400

    created tests for find_max

--- week_03_python_2/problems.py
+++ week_03_python_2/problems.py
@@ -2,6 +2,9 @@ def reverse_string(str):
     return str[::-1]
 
 def find_max(lst):
+    if len(lst) == 0:
+        return None
+    lst.sort()
     return lst[-1]
 
 def is_palindrome(str):
--- week_03_python_2/test/test_find_max.py
+++ week_03_python_2/test/test_find_max.py
@@ -3,3 +3,14 @@ from problems import find_max
 
 def test_find_max():
     assert find_max([1, 5, 3, 9]) == 9
+
+def test_empty():
+    assert find_max([]) == None
+
+def test_str():
+    with pytest.raises(AttributeError):
+        find_max("hello")
+
+def test_int():
+    with pytest.raises(TypeError):
+        find_max(1)
