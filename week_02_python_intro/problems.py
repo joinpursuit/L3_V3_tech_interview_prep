575ea0e903defb1f305da41cb6835218259074eb
yanakolajara <yanakolajara@pursuit.org>
2025-05-14 23:16:09 -0400

    solved is_palindrome problem

--- week_02_python_intro/problems.py
+++ week_02_python_intro/problems.py
@@ -5,4 +5,11 @@ def find_max(lst):
     return lst[-1]
 
 def is_palindrome(word):
-    pass
+    pointer1 = 0
+    pointer2 = len(word) - 1
+    while pointer1 < pointer2:
+        if word[pointer1] != word[pointer2]:
+            return False
+        pointer1 += 1
+        pointer2 -= 1
+    return True
